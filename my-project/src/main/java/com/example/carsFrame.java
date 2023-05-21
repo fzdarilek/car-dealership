a0f2cc06f96513e81dd700536b755fb75358e685
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-05-21 21:36:07 +0200

    update: carsframe

--- my-project/src/main/java/com/example/carsFrame.java
+++ my-project/src/main/java/com/example/carsFrame.java
@@ -1,9 +1,13 @@
+package com.example;
+
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JScrollPane;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,10 +21,12 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class carsFrame {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("carsJframe"); // Set the title of the JFrame
+    private static final String JSON_FILE_PATH = "carsList.json";
 
-        JTextArea textArea = new JTextArea(); // Create a JTextArea to display the JSON data
+    public static void main(String[] args) {
+        final JFrame frame = new JFrame("carsJframe"); // Set the title of the JFrame
+
+        final JTextArea textArea = new JTextArea(); // Create a JTextArea to display the JSON data
         JScrollPane scrollPane = new JScrollPane(textArea); // Wrap the JTextArea in a JScrollPane for scrolling
 
         frame.add(scrollPane, BorderLayout.CENTER); // Add the JScrollPane to the center of the JFrame
@@ -43,18 +49,8 @@ public class carsFrame {
 
         // Read the JSON file and display its contents in the JTextArea
         try {
-            BufferedReader reader = new BufferedReader(new FileReader("carsList.json"));
-
-            String line;
-            StringBuilder jsonContent = new StringBuilder();
-            while ((line = reader.readLine()) != null) {
-                jsonContent.append(line);
-                jsonContent.append("\n");
-            }
-
-            reader.close();
-
-            textArea.setText(jsonContent.toString());
+            String jsonContent = readJsonFile(JSON_FILE_PATH);
+            textArea.setText(jsonContent);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -62,41 +58,99 @@ public class carsFrame {
         // Action listener for the "Add Car" button
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                try {
-                    // Get the existing JSON content from the JTextArea
-                    String jsonContent = textArea.getText();
+                // Create a bubble window for user input
+                JPanel panel = new JPanel();
+                panel.setLayout(new BorderLayout());
 
-                    // Parse the JSON content as a JSONArray
-                    JSONArray jsonArray = new JSONArray(jsonContent);
+                // Create labels and text fields for each input field
+                JLabel brandLabel = new JLabel("Brand:");
+                JTextField brandField = new JTextField(10);
+                panel.add(brandLabel, BorderLayout.WEST);
+                panel.add(brandField, BorderLayout.EAST);
 
-                    // Create a new car JSONObject
-                    JSONObject newCar = new JSONObject();
-                    JSONObject carObject = new JSONObject();
-                    carObject.put("Brand", "");
-                    carObject.put("VIN", "");
-                    carObject.put("name", "");
-                    carObject.put("model", "");
-                    carObject.put("year", "");
-                    carObject.put("price", "");
-                    newCar.put("Car", carObject);
+                JLabel vinLabel = new JLabel("VIN:");
+                JTextField vinField = new JTextField(10);
+                panel.add(vinLabel, BorderLayout.WEST);
+                panel.add(vinField, BorderLayout.EAST);
 
-                    // Append the new car JSONObject to the existing JSONArray
-                    jsonArray.put(newCar);
+                JLabel nameLabel = new JLabel("Name:");
+                JTextField nameField = new JTextField(10);
+                panel.add(nameLabel, BorderLayout.WEST);
+                panel.add(nameField, BorderLayout.EAST);
 
-                    // Convert the updated JSONArray back to a formatted JSON string
-                    String updatedJsonContent = jsonArray.toString(4);
+                JLabel modelLabel = new JLabel("Model:");
+                JTextField modelField = new JTextField(10);
+                panel.add(modelLabel, BorderLayout.WEST);
+                panel.add(modelField, BorderLayout.EAST);
 
-                    // Save the updated JSON content to the file
-                    BufferedWriter writer = new BufferedWriter(new FileWriter("carsList.json"));
-                    writer.write(updatedJsonContent);
-                    writer.close();
+                JLabel yearLabel = new JLabel("Year:");
+                JTextField yearField = new JTextField(10);
+                panel.add(yearLabel, BorderLayout.WEST);
+                panel.add(yearField, BorderLayout.EAST);
 
-                    JOptionPane.showMessageDialog(frame, "Car added and saved successfully!");
-                } catch (JSONException | IOException ex) {
-                    ex.printStackTrace();
-                    JOptionPane.showMessageDialog(frame, "Failed to add and save the car!");
+                JLabel priceLabel = new JLabel("Price:");
+                JTextField priceField = new JTextField(10);
+                panel.add(priceLabel, BorderLayout.WEST);
+                panel.add(priceField, BorderLayout.EAST);
+
+                // Show the bubble window and wait for user input
+                int result = JOptionPane.showConfirmDialog(frame, panel, "Add Car", JOptionPane.OK_CANCEL_OPTION);
+                if (result == JOptionPane.OK_OPTION) {
+                    try {
+                        // Get the existing JSON content from the JTextArea
+                        String jsonContent = textArea.getText();
+
+                        // Parse the JSON content as a JSONArray
+                        JSONArray jsonArray = new JSONArray(jsonContent);
+
+                        // Create a new car JSONObject
+                        JSONObject newCar = new JSONObject();
+                        JSONObject carObject = new JSONObject();
+                        carObject.put("Brand", brandField.getText());
+                        carObject.put("VIN", vinField.getText());
+                        carObject.put("name", nameField.getText());
+                        carObject.put("model", modelField.getText());
+                        carObject.put("year", yearField.getText());
+                        carObject.put("price", priceField.getText());
+                        newCar.put("Car", carObject);
+
+                        // Append the new car JSONObject to the existing JSONArray
+                        jsonArray.put(newCar);
+
+                        // Convert the updated JSONArray back to a formatted JSON string
+                        String updatedJsonContent = jsonArray.toString(4);
+
+                        // Save the updated JSON content to the file
+                        writeJsonFile(JSON_FILE_PATH, updatedJsonContent);
+
+                        // Update the JTextArea with the updated JSON content
+                        textArea.setText(updatedJsonContent);
+
+                        JOptionPane.showMessageDialog(frame, "Car added and saved successfully!");
+                    } catch (JSONException | IOException ex) {
+                        ex.printStackTrace();
+                        JOptionPane.showMessageDialog(frame, "Failed to add and save the car!");
+                    }
                 }
             }
         });
+    }
+
+    private static String readJsonFile(String filePath) throws IOException {
+        StringBuilder jsonContent = new StringBuilder();
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            jsonContent.append(line);
+            jsonContent.append("\n");
+        }
+        reader.close();
+        return jsonContent.toString();
+    }
+
+    private static void writeJsonFile(String filePath, String jsonContent) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(filePath));
+        writer.write(jsonContent);
+        writer.close();
     }
 }
