bffb7bdae23d6037b7bb482481f954024d8962cc
phrenkie-caibi <95527652+phrenkie@users.noreply.github.com>
2023-05-27 15:03:45 +0200

    change: look

--- my-project/src/main/java/com/example/carsFrame.java
+++ my-project/src/main/java/com/example/carsFrame.java
@@ -34,22 +34,22 @@ public class carsFrame {
     private static JFrame frame;
     private static JTextArea textArea;
     private static JComboBox<String> comboBox;
+    //private static JPanel addCarPanel;
 
     public static void main(String[] args) {
         frame = new JFrame("Cars Dealership");
         textArea = new JTextArea();
         JScrollPane scrollPane = new JScrollPane(textArea);
-        frame.add(scrollPane, BorderLayout.CENTER);
+        frame.add(scrollPane, BorderLayout.SOUTH);
+
+        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         JToggleButton welcomeButton = new JToggleButton("Welcome");
         JToggleButton addCarButton = new JToggleButton("Add car");
         JToggleButton seeCarsButton = new JToggleButton("Show Car");
-        
-
-        JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         buttonPanel.add(welcomeButton);
         buttonPanel.add(addCarButton);
         buttonPanel.add(seeCarsButton);
-        frame.add(buttonPanel, BorderLayout.SOUTH);
+        frame.add(buttonPanel, BorderLayout.CENTER);
 
         int width = 800;
         int height = 600;
@@ -75,112 +75,140 @@ public class carsFrame {
             }
         });
     }
-
-    private static void addCar() {
-        final JPanel inputPanel = new JPanel();
-        inputPanel.setLayout(new GridLayout(6, 2));
-
-        JLabel brandLabel = new JLabel("Brand:");
-        JTextField brandField = new JTextField(10);
-        inputPanel.add(brandLabel);
-        inputPanel.add(brandField);
-
-        JLabel vinLabel = new JLabel("VIN:");
-        JTextField vinField = new JTextField(10);
-        inputPanel.add(vinLabel);
-        inputPanel.add(vinField);
-
-        JLabel nameLabel = new JLabel("Name:");
-        JTextField nameField = new JTextField(10);
-        inputPanel.add(nameLabel);
-        inputPanel.add(nameField);
-
-        JLabel modelLabel = new JLabel("Model:");
-        JTextField modelField = new JTextField(10);
-        inputPanel.add(modelLabel);
-        inputPanel.add(modelField);
-
-        JLabel yearLabel = new JLabel("Year:");
-        JTextField yearField = new JTextField(10);
-        inputPanel.add(yearLabel);
-        inputPanel.add(yearField);
-
-        JLabel priceLabel = new JLabel("Price:");
-        JTextField priceField = new JTextField(10);
-        inputPanel.add(priceLabel);
-        inputPanel.add(priceField);
-
-        JLabel imageLabel = new JLabel("Image:");
-        JButton chooseImageButton = new JButton("Choose Image");
-        final JLabel selectedImageLabel = new JLabel();
-        inputPanel.add(imageLabel);
-        inputPanel.add(chooseImageButton);
-        inputPanel.add(selectedImageLabel);
-
-        chooseImageButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JFileChooser fileChooser = new JFileChooser();
-                FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG Images", "png");
-                fileChooser.setFileFilter(filter);
-                int result = fileChooser.showOpenDialog(inputPanel);
-                if (result == JFileChooser.APPROVE_OPTION) {
-                    File selectedFile = fileChooser.getSelectedFile();
-                    selectedImageLabel.setText(selectedFile.getAbsolutePath());
+        private static void addCar() {
+            // Create the input fields and labels
+            JLabel brandLabel = new JLabel("Brand:");
+            JTextField brandField = new JTextField(10);
+        
+            JLabel vinLabel = new JLabel("VIN:");
+            JTextField vinField = new JTextField(10);
+        
+            JLabel nameLabel = new JLabel("Name:");
+            JTextField nameField = new JTextField(10);
+        
+            JLabel modelLabel = new JLabel("Model:");
+            JTextField modelField = new JTextField(10);
+        
+            JLabel yearLabel = new JLabel("Year:");
+            JTextField yearField = new JTextField(10);
+        
+            JLabel priceLabel = new JLabel("Price:");
+            JTextField priceField = new JTextField(10);
+        
+            JLabel imageLabel = new JLabel("Image:");
+            JButton chooseImageButton = new JButton("Choose Image");
+            final JLabel selectedImageLabel = new JLabel();
+        
+            // Set the layout for the input panel
+            JPanel inputPanel = new JPanel();
+            inputPanel.setLayout(new BoxLayout(inputPanel, BoxLayout.Y_AXIS));
+        
+            // Create a separate panel for each row of input fields and labels
+            JPanel brandPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            brandPanel.add(brandLabel);
+            brandPanel.add(brandField);
+            inputPanel.add(brandPanel);
+        
+            JPanel vinPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            vinPanel.add(vinLabel);
+            vinPanel.add(vinField);
+            inputPanel.add(vinPanel);
+        
+            JPanel namePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            namePanel.add(nameLabel);
+            namePanel.add(nameField);
+            inputPanel.add(namePanel);
+        
+            JPanel modelPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            modelPanel.add(modelLabel);
+            modelPanel.add(modelField);
+            inputPanel.add(modelPanel);
+        
+            JPanel yearPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            yearPanel.add(yearLabel);
+            yearPanel.add(yearField);
+            inputPanel.add(yearPanel);
+        
+            JPanel pricePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            pricePanel.add(priceLabel);
+            pricePanel.add(priceField);
+            inputPanel.add(pricePanel);
+        
+            JPanel imagePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+            imagePanel.add(imageLabel);
+            imagePanel.add(chooseImageButton);
+            inputPanel.add(imagePanel);
+            inputPanel.add(selectedImageLabel);
+        
+            // Add an action listener to the chooseImageButton
+            chooseImageButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser fileChooser = new JFileChooser();
+                    FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG Images", "png");
+                    fileChooser.setFileFilter(filter);
+                    int result = fileChooser.showOpenDialog(frame);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        File selectedFile = fileChooser.getSelectedFile();
+                        selectedImageLabel.setText(selectedFile.getAbsolutePath());
+                    }
                 }
-            }
-        });
+            });
         
-        int result = JOptionPane.showConfirmDialog(frame, inputPanel, "Add Car", JOptionPane.OK_CANCEL_OPTION);
-        if (result == JOptionPane.OK_OPTION) {
-            // Retrieve the car information
-            String brand = brandField.getText();
-            String vin = vinField.getText();
-            String name = nameField.getText();
-            String model = modelField.getText();
-            String year = yearField.getText();
-            String price = priceField.getText();
-            String imagePath = selectedImageLabel.getText();
+            // Show the input panel in a JOptionPane
+            int result = JOptionPane.showConfirmDialog(frame, inputPanel, "Add Car", JOptionPane.OK_CANCEL_OPTION);
+            if (result == JOptionPane.OK_OPTION) {
+                // Retrieve the car information
+                String brand = brandField.getText();
+                String vin = vinField.getText();
+                String name = nameField.getText();
+                String model = modelField.getText();
+                String year = yearField.getText();
+                String price = priceField.getText();
+                String imagePath = selectedImageLabel.getText();
+        
+                // Validate the input fields
+                if (brand.isEmpty() || vin.isEmpty() || name.isEmpty() || model.isEmpty() || year.isEmpty() || price.isEmpty() || imagePath.isEmpty()) {
+                    JOptionPane.showMessageDialog(frame, "Please fill in all the fields.");
+                    addCar();
+                    return;
+                }
+        
+                if (vin.length() != 17 || !vin.matches("[A-HJ-NPR-Z0-9]{17}")) {
+                    JOptionPane.showMessageDialog(frame, "Invalid VIN. Please enter a valid VIN.");
+                    addCar();
+                    return;
+                }
+        
+                try {
+                    float floatValue = Float.parseFloat(price);
+                    if (floatValue < 0) {
+                        JOptionPane.showMessageDialog(frame, "Invalid price. Please enter a valid price.");
+                        addCar();
+                        return;
+                    }
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(frame, "Invalid price. Please enter a valid price.");
+                    addCar();
+                    return;
+                }
         
 
-        if (brand.isEmpty() || vin.isEmpty() || name.isEmpty() || model.isEmpty() || year.isEmpty() || price.isEmpty() || imagePath.isEmpty()) {
-            JOptionPane.showMessageDialog(frame, "Please fill in all the fields.");
-            return;
-        }
-
-        if (vin.length() != 17 || !vin.matches("[A-HJ-NPR-Z0-9]{17}")) {
-            JOptionPane.showMessageDialog(frame, "Invalid VIN. Please enter a valid VIN.");
-            return;
-        }
-
-        try {
-            float floatValue = Float.parseFloat(price);
-            if (floatValue < 0) {
-                JOptionPane.showMessageDialog(frame, "Invalid price. Please enter a valid price.");
-                return;
-            }
-        } catch (NumberFormatException e) {
-            JOptionPane.showMessageDialog(frame, "Invalid price. Please enter a valid price.");
-            return;
-        }
-        
-            
             String currentDirectory = System.getProperty("user.dir");
             String destinationPath = currentDirectory + "\\resources\\addedImages";
-        
+
             File destinationFolder = new File(destinationPath);
             int imageCount = destinationFolder.listFiles().length;
-        
+
             String newImageName = "image" + (imageCount + 1) + ".png";
             Path destination = Paths.get(destinationPath + File.separator + newImageName);
-        
+
             try {
                 BufferedImage inputImage = ImageIO.read(new File(imagePath));
                 File destinationFile = new File(destination.toString());
                 ImageIO.write(inputImage, "png", destinationFile);
                 System.out.println("Image saved: " + destinationFile.getAbsolutePath());
-        
-                
+
                 JSONObject carData = new JSONObject();
                 carData.put("Brand", brand);
                 carData.put("VIN", vin);
@@ -188,10 +216,10 @@ public class carsFrame {
                 carData.put("Model", model);
                 carData.put("Year", year);
                 carData.put("Price", price);
-        
+
                 final JSONArray carsArray;
                 final JSONObject carsData;
-        
+
                 if (Files.exists(Paths.get(JSON_FILE_PATH))) {
                     String fileContent = new String(Files.readAllBytes(Paths.get(JSON_FILE_PATH)));
                     carsData = new JSONObject(fileContent);
@@ -204,11 +232,11 @@ public class carsFrame {
                     carsData = new JSONObject();
                     carsArray = new JSONArray();
                 }
-        
+
                 carsArray.put(carData);
                 carsData.put("Cars", carsArray);
                 Files.write(Paths.get(JSON_FILE_PATH), carsData.toString().getBytes());
-        
+
                 JOptionPane.showMessageDialog(frame, "Car added successfully!");
             } catch (JSONException | IOException ex) {
                 ex.printStackTrace();
@@ -216,8 +244,6 @@ public class carsFrame {
             }
         }
     }
-        
-
     private static void showCars() {
         try {
             if (Files.exists(Paths.get(JSON_FILE_PATH))) {
